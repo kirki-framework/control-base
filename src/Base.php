f8104c63734509642ae402bc0398f2c75514beb9
Ari <aristath@gmail.com>
2019-02-24 12:10:38 +0200

    kirki-script no longer needed

--- src/Base.php
+++ src/Base.php
@@ -103,15 +103,6 @@ class Base extends \WP_Customize_Control {
 		// The Kirki plugin URL.
 		$kirki_url = trailingslashit( Kirki::$url );
 
-		// Enqueue ColorPicker.
-		wp_enqueue_script( 'wp-color-picker-alpha', trailingslashit( Kirki::$url ) . 'assets/vendor/wp-color-picker-alpha/wp-color-picker-alpha.js', array( 'wp-color-picker' ), KIRKI_VERSION, true );
-		wp_enqueue_style( 'wp-color-picker' );
-
-		// Enqueue selectWoo.
-		wp_enqueue_script( 'selectWoo', trailingslashit( Kirki::$url ) . 'assets/vendor/selectWoo/js/selectWoo.full.js', array( 'jquery' ), '1.0.1', true );
-		wp_enqueue_style( 'selectWoo', trailingslashit( Kirki::$url ) . 'assets/vendor/selectWoo/css/selectWoo.css', array(), '1.0.1' );
-		wp_enqueue_style( 'kirki-selectWoo', trailingslashit( Kirki::$url ) . 'assets/vendor/selectWoo/kirki.css', array(), KIRKI_VERSION );
-
 		$url = apply_filters(
 			'kirki_package_url_control_base',
 			trailingslashit( Kirki::$url ) . 'vendor/kirki-framework/control-base/src'
@@ -140,38 +131,6 @@ class Base extends \WP_Customize_Control {
 			],
 			KIRKI_VERSION,
 			false
-		);
-
-		// Enqueue the script.
-		wp_enqueue_script(
-			'kirki-script',
-			"{$kirki_url}controls/js/script{$suffix}.js",
-			array(
-				'jquery',
-				'customize-base',
-				'wp-color-picker-alpha',
-				'selectWoo',
-				'jquery-ui-button',
-				'jquery-ui-datepicker',
-			),
-			KIRKI_VERSION,
-			false
-		);
-
-		wp_localize_script(
-			'kirki-script',
-			'kirkiL10n',
-			array(
-				'isScriptDebug'        => ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ),
-				'noFileSelected'       => esc_html__( 'No File Selected', 'kirki' ),
-				'remove'               => esc_html__( 'Remove', 'kirki' ),
-				'default'              => esc_html__( 'Default', 'kirki' ),
-				'selectFile'           => esc_html__( 'Select File', 'kirki' ),
-				'standardFonts'        => esc_html__( 'Standard Fonts', 'kirki' ),
-				'googleFonts'          => esc_html__( 'Google Fonts', 'kirki' ),
-				'defaultCSSValues'     => esc_html__( 'CSS Defaults', 'kirki' ),
-				'defaultBrowserFamily' => esc_html__( 'Default Browser Font-Family', 'kirki' ),
-			)
 		);
 
 		$suffix = str_replace( '.min', '', $suffix );
